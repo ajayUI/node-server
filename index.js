6288b8fc2f3eee411e0bdbc2ec771c2f46c96aba
Ajay Dubey <hellodubey.ajay@gmail.com>
2025-03-03 13:32:27 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+//const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send({ hi : "Hello World"});
+})
+
+app.listen(PORT, () => {    console.log(`Server is running on port ${PORT}`);
+})
